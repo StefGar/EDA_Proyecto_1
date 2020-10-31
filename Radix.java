7b401e4337c60b4df5ba48de796836e7fb9986d4
francis12321 <73733563+francis12321@users.noreply.github.com>
2020-10-31 00:45:41 -0600

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -10,5 +10,34 @@ package OrdenamientoExterno;
  * @author faang
  */
 public class Radix {
-    
+    int separar(int value, int iteracion) {
+		int x=0;
+		switch(iteracion) {
+		case 0:
+			x = value%10; //Esto serviría para obtener las unidades de la primera iteracion
+			break;
+		case 1:
+			x = ((((value%100000)%10000)%1000)%100)/10; //Esto para las decenas
+			break;
+		case 2:
+			x = (((value%100000)%10000)%1000)/100;  //centenas
+			break;
+		case 3:
+			x = ((value%100000)%10000)/1000;    //millares
+			break;
+		case 4:
+			x = (value%100000)/10000;
+			break;
+		case 5:
+			x = value/100000;
+			break;
+		}
+		return x;
+	}
+    void Sort (int value) {
+		for(int i=0; i<6; i++) {
+			int digito = separar(value,i);
+			escribir(digito);
+		}
+	}
 }
