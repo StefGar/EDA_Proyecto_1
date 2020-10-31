079f3f0b1df7e540919c9c266ed1e0afdf7f2df9
Stefany García <faangaar@gmail.com>
2020-10-30 22:57:55 -0600

    Update and rename Menus.java to Utilidades.java

---
+++ Utilidades.java
@@ -0,0 +1,18 @@
+package OrdenamientoExterno;
+
+public class Utilidades {
+    public static void menu1(){
+        System.out.println("Elige el método de ordenamiento");
+        System.out.println("1 - Polifase");
+        System.out.println("2 - Mezcla equilibrada");
+        System.out.println("3 - Radix");
+        System.out.println("0 - Salir");
+    }
+    public static void menu2(){
+        System.out.println("Criterio a ordenar:1");
+        System.out.println("1 - Apellido");
+        System.out.println("2 - Nombre");
+        System.out.println("3 - No. de cuenta");
+        System.out.println("0 - Salir");
+    }
+}
